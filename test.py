d9fd740b56ac9241c653cc04ec947928dfcf4a97
Chris <kohler.nuernberg@gmail.com>
2026-05-21 08:37:50 +0200

    weitere änderung

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 print("zweiter Anlauf")
 
-liste = ['H', 'l', 'o', 'a']
-gruss = liste[0] + liste[3] + liste[1] + liste[1] + liste[2] 
- 
-print(gruss)
+liste = ['H', 'l', 'o', 'a', 'e', 'W', 't', 'l']
+gruss = [0, 3, 1, 1, 2]
+gruss2 = [5, 4, 7, 6]
+
+wort = "".join(liste[i] for i in gruss)
+wort2 = "".join (liste[i] for i in gruss2)
+
+print(wort + ' ' + wort2)
